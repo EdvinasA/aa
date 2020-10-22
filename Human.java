633730a7759d8c1847edefb86bc876cff85fdc49
Edvinas <edvinasalimas98@gmail.com>
2020-10-22 20:08:37 +0300

    Human

--- Human.java
+++ Human.java
@@ -1,4 +1,22 @@
 package com.company1;
 
+
 public class Human {
+
+    private String name;
+    private String lastName;
+    private boolean lungs;
+    private boolean heart;
+    private boolean stomach;
+
+
+    private boolean eyes;
+    private boolean legs;
+    private boolean arms;
+    private boolean hair;
+    private boolean fingers;
+    private boolean skin;
+    private boolean muscles;
+
+
 }
