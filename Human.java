e707c5201f9517b05f4c7db9394cf4c17f8fcec8
Edvinas <edvinasalimas98@gmail.com>
2020-10-22 20:12:13 +0300

    Human

--- Human.java
+++ Human.java
@@ -5,18 +5,49 @@ public class Human {
 
     private String name;
     private String lastName;
-    private boolean lungs;
-    private boolean heart;
-    private boolean stomach;
+    private int lungs;
+    private int heart;
+    private int stomach;
 
 
-    private boolean eyes;
-    private boolean legs;
-    private boolean arms;
+    private int eyes;
+    private int legs;
+    private int arms;
     private boolean hair;
-    private boolean fingers;
+    private int fingers;
     private boolean skin;
     private boolean muscles;
 
+    public Human(String name, String lastName, int lungs, int heart, int stomach, int eyes, int legs, int arms, boolean hair, int fingers, boolean skin, boolean muscles) {
+        this.name = name;
+        this.lastName = lastName;
+        this.lungs = lungs;
+        this.heart = heart;
+        this.stomach = stomach;
+        this.eyes = eyes;
+        this.legs = legs;
+        this.arms = arms;
+        this.hair = hair;
+        this.fingers = fingers;
+        this.skin = skin;
+        this.muscles = muscles;
+    }
 
+    @Override
+    public String toString() {
+        return "Human{" +
+                "name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", lungs=" + lungs +
+                ", heart=" + heart +
+                ", stomach=" + stomach +
+                ", eyes=" + eyes +
+                ", legs=" + legs +
+                ", arms=" + arms +
+                ", hair=" + hair +
+                ", fingers=" + fingers +
+                ", skin=" + skin +
+                ", muscles=" + muscles +
+                '}';
+    }
 }
--- Main.java
+++ Main.java
@@ -4,5 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Human human = new Human("a", "a", 2,2,2,2,4,3,true,12,true, true);
+
+        System.out.println(human);
     }
 }
